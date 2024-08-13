dc7d806a2d8070c43912cf4d23b2d8f6d0e5b639
Prem_Kumar <krprem514@gmail.com>
2024-08-14 00:47:02 +0530

    ready of deployment

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
+import os
 from app import create_app
 
 flask_app = create_app()
 
 if __name__ == '__main__':
-    flask_app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    flask_app.run(host='0.0.0.0', port=port, debug=True)
 
